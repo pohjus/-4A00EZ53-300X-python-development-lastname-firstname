2bbff2530b19a670b47c5513593969a06615ad81
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-01-13 19:03:17 +0200

    style fix

--- project/src/main.py
+++ project/src/main.py
@@ -3,9 +3,11 @@
 Short explanation of the file and the project.
 """
 
+
 def output_guide():
     """Outputs user gude for the application"""
     print("To use the app ... ")
+
 
 def output_author():
     """Outputs welcome message and author name"""
@@ -17,5 +19,6 @@ def main():
     """The main function that starts the app"""
     output_author()
     output_guide()
+
 
 main()
