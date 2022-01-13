f8643f44ff09fd5044bf6230a630da9d8dafc1ef
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-01-13 16:32:29 +0200

    Added README template and proper dir structure

---
+++ project/src/main.py
@@ -0,0 +1,21 @@
+"""My Project
+
+Short explanation of the file and the project.
+"""
+
+def output_guide():
+    """Outputs user gude for the application"""
+    print("To use the app ... ")
+
+def output_author():
+    """Outputs welcome message and author name"""
+    print("Welcome to my Project!")
+    print("Author: Firstname Lastname")
+
+
+def main():
+    """The main function that starts the app"""
+    output_author()
+    output_guide()
+
+main()
