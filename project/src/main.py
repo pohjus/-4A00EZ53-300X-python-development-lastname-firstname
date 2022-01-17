34c96d5f08929de7c1c0094c49d32dc73b2eae7b
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-01-17 19:05:05 +0200

    ADD project test files and documentation files (html)

--- project/src/main.py
+++ project/src/main.py
@@ -1,24 +1,9 @@
-"""My Project
-
-Short explanation of the file and the project.
-"""
-
-
-def output_guide():
-    """Outputs user gude for the application"""
-    print("To use the app ... ")
-
-
-def output_author():
-    """Outputs welcome message and author name"""
-    print("Welcome to my Project!")
-    print("Author: Firstname Lastname")
+from ui import get_title
 
 
 def main():
     """The main function that starts the app"""
-    output_author()
-    output_guide()
+    print(get_title())
 
 
 main()
